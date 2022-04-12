bfa5078fa3094997b7488825be19d824a5d11978
Roelan Geil <bblcitdev@gmail.com>
2022-04-12 11:01:43 +0800

    initial project

---
+++ src/pages/tables/index.js
@@ -0,0 +1,86 @@
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+
+import MDBox from "atoms/MDBox";
+import MDTypography from "atoms/MDTypography";
+
+import DashboardLayout from "organisms/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "organisms/Navbars/DashboardNavbar";
+import Footer from "organisms/Footer";
+import DataTable from "organisms/Tables/DataTable";
+
+// Data
+import authorsTableData from "pages/tables/data/authorsTableData";
+import projectsTableData from "pages/tables/data/projectsTableData";
+
+function Tables() {
+  const { columns, rows } = authorsTableData();
+  const { columns: pColumns, rows: pRows } = projectsTableData();
+
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox pt={6} pb={3}>
+        <Grid container spacing={6}>
+          <Grid item xs={12}>
+            <Card>
+              <MDBox
+                mx={2}
+                mt={-3}
+                py={3}
+                px={2}
+                variant="gradient"
+                bgColor="info"
+                borderRadius="lg"
+                coloredShadow="info"
+              >
+                <MDTypography variant="h6" color="white">
+                  Authors Table
+                </MDTypography>
+              </MDBox>
+              <MDBox pt={3}>
+                <DataTable
+                  table={{ columns, rows }}
+                  isSorted={false}
+                  entriesPerPage={false}
+                  showTotalEntries={false}
+                  noEndBorder
+                />
+              </MDBox>
+            </Card>
+          </Grid>
+          <Grid item xs={12}>
+            <Card>
+              <MDBox
+                mx={2}
+                mt={-3}
+                py={3}
+                px={2}
+                variant="gradient"
+                bgColor="info"
+                borderRadius="lg"
+                coloredShadow="info"
+              >
+                <MDTypography variant="h6" color="white">
+                  Projects Table
+                </MDTypography>
+              </MDBox>
+              <MDBox pt={3}>
+                <DataTable
+                  table={{ columns: pColumns, rows: pRows }}
+                  isSorted={false}
+                  entriesPerPage={false}
+                  showTotalEntries={false}
+                  noEndBorder
+                />
+              </MDBox>
+            </Card>
+          </Grid>
+        </Grid>
+      </MDBox>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default Tables;
