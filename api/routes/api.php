71f10866bc53c0f066f7b06ed49e1e4eaed04719
Roelan Eroy <roelaneroy@gmail.com>
2022-10-07 14:16:58 +0800

    API: update routes prefix

--- api/routes/api.php
+++ api/routes/api.php
@@ -16,15 +16,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Public routes
-Route::get('/auth/login', [AuthController::class, 'login'])->name('login');
-Route::post('/auth/register', [AuthController::class, 'register']);
-Route::post('/auth/reset-password', [AuthController::class, 'reset'])->name('password.reset');
-Route::post('/auth/change-password', [AuthController::class, 'change']);
+Route::prefix('auth')->group(function () {
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/reset-password', [AuthController::class, 'reset'])->name('password.reset');
+    Route::post('/change-password', [AuthController::class, 'change']);
+});
 
 // Protected routes
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/auth/logout', [AuthController::class, 'logout']);
-    Route::get('/auth/is-authenticated', [AuthController::class, 'isAuthenticated']);
-    Route::get('/inventory/warehouse-list', [InventoryController::class, 'warehouseList']);
-    Route::get('/inventory/table', [InventoryController::class, 'table']);
+    Route::prefix('auth')->group(function () {
+        Route::get('/logout', [AuthController::class, 'logout']);
+        Route::get('/is-authenticated', [AuthController::class, 'isAuthenticated']);
+    });
+
+    Route::prefix('inventory')->group(function () {
+        Route::get('/warehouse-list', [InventoryController::class, 'warehouseList']);
+        Route::get('/table', [InventoryController::class, 'table']);
+    });
 });
