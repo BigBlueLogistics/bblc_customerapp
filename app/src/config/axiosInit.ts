4d4f35697ac748798b8b132f37ee6407c6836770
Roelan Eroy <roelaneroy@gmail.com>
2022-11-07 16:49:08 +0800

    implement axios interceptors

--- app/src/config/axiosInit.ts
+++ app/src/config/axiosInit.ts
@@ -5,13 +5,31 @@ class AxiosInit {
   private instance: AxiosInstance;
 
   constructor() {
-    this.instance = axios.create({
+    const init = axios.create({
       baseURL: urls().apiUrl,
       withCredentials: true,
       headers: {
         Accept: "application/json",
       },
     });
+
+    // intercept api response
+    init.interceptors.response.use(
+      (success) => {
+        return success;
+      },
+      (err) => {
+        // Use api message if exists.
+        const error = { ...err };
+        if (error?.response?.data?.message) {
+          error.message = error.response.data.message;
+        }
+
+        return Promise.reject(error);
+      }
+    );
+
+    this.instance = init;
   }
 
   public axios() {
