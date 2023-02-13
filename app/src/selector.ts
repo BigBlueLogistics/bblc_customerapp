14763dfdba2d0f4e5747c81fac2766c6b8427b94
Roelan Geil <roelaneroy@gmail.com>
2023-02-13 09:10:32 +0800

    Add state name customerCode in selector function

--- app/src/selector.ts
+++ app/src/selector.ts
@@ -4,9 +4,11 @@ import { signIn } from "redux/auth/action";
 export default () => {
   const { successfulRequests } = useAppSelector((state) => state.auth);
 
-  const { role_name: accountRole } = successfulRequests[signIn.fulfilled.type]?.data || {};
+  const { role_name: accountRole, customer_code: customerCode } =
+    successfulRequests[signIn.fulfilled.type]?.data || {};
 
   return {
     accountRole,
+    customerCode,
   };
 };
