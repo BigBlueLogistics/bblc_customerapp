d34d70d98e4c61ad06bc5145f1951957d2dc1d6c
Roelan Geil <roelaneroy@gmail.com>
2023-02-17 09:16:25 +0800

    Update form schema validation

--- app/src/pages/Orders/components/Form/validationSchema.ts
+++ app/src/pages/Orders/components/Form/validationSchema.ts
@@ -1,6 +1,15 @@
 import * as yup from "yup";
 
 export default yup.object({
-  ref_number: yup.number().typeError("not a number").required("required"),
+  ref_number: yup
+    .number()
+    .typeError("not a number")
+    .required("required")
+    .test({
+      name: "exact-digits",
+      message: "Number of digits must 12",
+      exclusive: true,
+      test: (value) => String(value).length === 12,
+    }),
   source_wh: yup.string().required("required"),
 });
