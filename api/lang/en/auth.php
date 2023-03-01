9577e0053e24d5ed306676968625e6cc71032b31
Roelan Geil <roelaneroy@gmail.com>
2023-03-01 15:46:02 +0800

    Update success message in sign-up

--- api/lang/en/auth.php
+++ api/lang/en/auth.php
@@ -19,7 +19,7 @@ return [
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
     'disabled' => 'These account has been disabled.',
     'not-verified' => 'These Account is not yet verified. Please check your email we\'ve sent for verification.',
-    'register' => 'Account has been successfully registered.',
+    'register' => 'Your registration details will be verified by our team. Once validated, an email will be sent to your registered email address for your temporary password.',
     'register-failed' => 'Account failed to register.',
     'register-exists' => 'Email Address has been exists.',
     'logout' => 'Successfully log-out',
